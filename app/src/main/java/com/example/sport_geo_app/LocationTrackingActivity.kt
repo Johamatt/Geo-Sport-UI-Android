498118f37bc1960681d881b6ef80e3072ec9d2d9
johamatt94 <johamatt94@gmail.com>
2024-03-24 19:24:03 +0200

    added user location tracker

--- app/src/main/java/com/example/sport_geo_app/LocationTrackingActivity.kt
+++ app/src/main/java/com/example/sport_geo_app/LocationTrackingActivity.kt
@@ -1,25 +1,17 @@
 package com.example.sport_geo_app
 
+
+
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import com.example.sport_geo_app.utils.BitmapUtils.bitmapFromDrawableRes
-import android.graphics.Color
 import android.widget.Toast
-import androidx.core.content.ContextCompat
+import androidx.activity.ComponentActivity
 import com.example.sport_geo_app.utils.LocationPermissionHelper
 import com.mapbox.android.gestures.MoveGestureDetector
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.ImageHolder
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
-import com.mapbox.maps.extension.style.expressions.dsl.generated.*
-import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.step
-import com.mapbox.maps.extension.style.layers.addLayer
-import com.mapbox.maps.extension.style.layers.generated.circleLayer
-import com.mapbox.maps.extension.style.layers.generated.symbolLayer
-import com.mapbox.maps.extension.style.sources.addSource
-import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
-import com.mapbox.maps.extension.style.utils.ColorUtils
+import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
 import com.mapbox.maps.plugin.LocationPuck2D
 import com.mapbox.maps.plugin.PuckBearing
 import com.mapbox.maps.plugin.gestures.OnMoveListener
@@ -27,10 +19,14 @@ import com.mapbox.maps.plugin.gestures.gestures
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
 import com.mapbox.maps.plugin.locationcomponent.location
+
+
 import java.lang.ref.WeakReference
 
-
-class MainActivity : ComponentActivity() {
+/**
+ * Tracks the user location on screen, simulates a navigation session.
+ */
+class LocationTrackingActivity : ComponentActivity() {
 
     private lateinit var locationPermissionHelper: LocationPermissionHelper
 
@@ -54,7 +50,6 @@ class MainActivity : ComponentActivity() {
 
         override fun onMoveEnd(detector: MoveGestureDetector) {}
     }
-
     private lateinit var mapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,20 +65,14 @@ class MainActivity : ComponentActivity() {
     private fun onMapReady() {
         mapView.mapboxMap.setCamera(
             CameraOptions.Builder()
-                .zoom(10.0)
-                .pitch(0.0)
+                .zoom(14.0)
                 .build()
-
         )
         mapView.mapboxMap.loadStyle(
             Style.STANDARD
         ) {
             initLocationComponent()
             setupGesturesListener()
-            addClusteredGeoJsonSource(it)
-            bitmapFromDrawableRes(this@MainActivity, R.drawable.ic_cross)?.let { bitmap ->
-                it.addImage(CROSS_ICON_ID, bitmap, true)
-            }
         }
     }
 
@@ -143,85 +132,5 @@ class MainActivity : ComponentActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         locationPermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-    private fun addClusteredGeoJsonSource(style: Style) {
-
-        // Add a new source from the GeoJSON data and set the 'cluster' option to true.
-        style.addSource(
-            geoJsonSource(GEOJSON_SOURCE_ID) {
-                data("http://10.0.2.2:3000/sportPlaces/map/findAllGeoJson")
-                cluster(true)
-                maxzoom(14)
-                clusterRadius(50)
-            }
-        )
-
-        // Creating a marker layer for single data points
-        style.addLayer(
-            symbolLayer("unclustered-points", GEOJSON_SOURCE_ID) {
-                iconImage(CROSS_ICON_ID)
-                iconSize(
-                    literal(1) // Set a fixed icon size or calculate size based on another property if available in your GeoJSON data
-                )
-
-                filter(
-                    has {
-                        literal("name")
-                    }
-                )
-            }
-        )
-
-        val layers = arrayOf(
-            intArrayOf(150, ContextCompat.getColor(this, R.color.red)),
-            intArrayOf(20, ContextCompat.getColor(this, R.color.green)),
-            intArrayOf(0, ContextCompat.getColor(this, R.color.blue))
-        )
-
-        // Add clusters' circles
-        style.addLayer(
-            circleLayer("clusters", GEOJSON_SOURCE_ID) {
-                circleColor(
-                    step(
-                        input = get("point_count"),
-                        output = literal(ColorUtils.colorToRgbaString(layers[2][1])),
-                        stops = arrayOf(
-                            literal(layers[1][0].toDouble()) to literal(ColorUtils.colorToRgbaString(layers[1][1])),
-                            literal(layers[0][0].toDouble()) to literal(ColorUtils.colorToRgbaString(layers[0][1]))
-                        )
-                    )
-                )
-                circleRadius(18.0)
-                filter(
-                    has("point_count")
-                )
-            }
-        )
-
-        style.addLayer(
-            symbolLayer("count", GEOJSON_SOURCE_ID) {
-                textField(
-                    format {
-                        formatSection(
-                            toString {
-                                get {
-                                    literal("point_count")
-                                }
-                            }
-                        )
-                    }
-                )
-                textSize(12.0)
-                textColor(Color.WHITE)
-                textIgnorePlacement(true)
-                textAllowOverlap(true)
-            }
-        )
-    }
-
-    companion object {
-        private const val GEOJSON_SOURCE_ID = "sportplaces"
-        private const val CROSS_ICON_ID = "cross-icon-id"
     }
 }
